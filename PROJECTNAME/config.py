a0f07ed4291638c4bee3d1e237e338c5b484c435
Brian Harbron <bharbron@grubhub.com>
2015-07-24 14:18:31 -0500

    Fixed dev logconfig path

--- PROJECTNAME/config.py
+++ PROJECTNAME/config.py
@@ -4,4 +4,4 @@ class DevelopmentConfig(object):
   SQLALCHEMY_DATABASE_URI = "postgresql://vagrant@localhost:5432/PROJECTNAME"
   DEBUG = True
   SECRET_KEY = os.environ.get("PROJECTNAME_SECRET_KEY", "foobar")
-  LOGCONFIG_PATH = "grubhubbub/logconfig.dev.json"
+  LOGCONFIG_PATH = "PROJECTNAME/logconfig.dev.json"
